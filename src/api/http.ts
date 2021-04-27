df0295a978c0c0b8f9c148384b814da0c3bae8c2
zzdoreen <zz1061831341@qq.com>
2021-04-27 16:23:52 +0800

    update: music-api url

--- src/api/http.ts
+++ src/api/http.ts
@@ -6,7 +6,7 @@ axios.defaults.timeout = 10000
 export function get(type: number, url: string, params: any) {
     switch (type) {
         // case 1: url = '/api' + url; break;
-        case 2: url = 'https://api.uomg.com/api/rand.music?format=json&sort=%E7%83%AD%E6%AD%8C%E6%A6%9C' + url; break;
+        case 2: url = 'https://api.uomg.com/api/rand.music?format=json&mid=2343249232' + url; break;
         case 3: url = 'https://api.imjad.cn/cloudmusic/' + url; break
     }
 
